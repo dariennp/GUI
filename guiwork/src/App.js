1a2ac53f6293d203d0adc26d5ab6093b9832ba73
shayparmar <ec22841@qmul.ac.uk>
2024-03-17 19:35:23 +0000

    draft2

--- guiwork/src/App.js
+++ guiwork/src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
 import Weather from './Weather'
-import Map from './Map';
-import WeatherRouter from './WeatherRouter'
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <RouterProvider router={WeatherRouter}></RouterProvider>
+        <Weather/>
       </header>
     </div>
   );
