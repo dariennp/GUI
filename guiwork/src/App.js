642f09b58305859710b4405f334eb34acfdee63a
Darien <112005901+dariennp@users.noreply.github.com>
2024-03-17 20:05:49 +0000

    Revert "draft2"

--- guiwork/src/App.js
+++ guiwork/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Weather from './Weather'
+import Map from './Map';
+import WeatherRouter from './WeatherRouter'
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Weather/>
+        <RouterProvider router={WeatherRouter}></RouterProvider>
       </header>
     </div>
   );
